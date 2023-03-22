95ca4231bfeea64979153f24ad2ba8bac05f69ee
Dzakwan Raditya <aditdzakwan77@gmail.om>
2023-03-22 10:24:50 +0700

    first commit

---
+++ bedalagi88/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main () {
+    Pertemuan 1
+    println ("Hello World")
+    println ("P")
+
+    print("Pekerjaan: ")
+   val pekerjaan = readLine()
+
+   print ("Alamat: ")
+   val alamat = readLine()
+
+   println("Pekerjaan kamu adalah $pekerjaan dan alamat kamu di $alamat")
+}
